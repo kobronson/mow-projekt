1db12e29379eb433879ab8607532eacb8c2061ef
Dominika Sawicka <domsawicka@gmail.com>
2014-01-12 22:41:40 +0100

    + Uporządkowane pliki z danymi + skrypt do wczytywania danych do R-a

---
+++ dane/prepareData.r
@@ -0,0 +1,2 @@
+spamnumericdata <- read.csv("SpamBase/spambase.csv", header= TRUE, sep = ",", row.names = NULL)
+spamliteraldata <- read.csv("SpamCollection/SMSSpamCollection.csv", header= TRUE, sep = "\t", row.names = NULL, quote = NULL)
